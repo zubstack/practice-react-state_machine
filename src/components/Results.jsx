ec8f38d21c7237b74b67c3389846a3302e8e84b2
Ariel Alejandro <mt.calderon.alejandro@gmail.com>
2023-09-20 15:30:02 -0500

    [working]: results

--- src/components/Results.jsx
+++ src/components/Results.jsx
@@ -1,7 +1,17 @@
-import React from "react";
+import Nav from "./Nav";
 
-function Results() {
-  return <div>Results</div>;
+function Results({ send }) {
+  return (
+    <>
+      <h2>Results:</h2>
+      <ul>
+        <li>Goods:2</li>
+        <li>Bads:2</li>
+        <li>Not awsered:2</li>
+      </ul>
+      <Nav send={send} action={"RETRY"} />
+    </>
+  );
 }
 
 export default Results;
