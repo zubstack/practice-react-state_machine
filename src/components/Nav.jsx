36660a20e9cbdecc804e653ca701dcd669c8b891
Ariel Alejandro <mt.calderon.alejandro@gmail.com>
2023-09-20 17:02:05 -0500

    [fix]: exit question option

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,12 +1,19 @@
 import "./Nav.css";
 
 function Nav({ action, send }) {
+  const isQuestion = action === "CONTINUE" || "FINISH";
+
   function next() {
     send(action);
   }
+  function exit() {
+    send("EXIT");
+  }
   return (
     <nav className="navbar__container">
-      <button className="button__back">Back</button>
+      <button onClick={exit} className="button__exit">
+        EXIT
+      </button>
       <button onClick={next} className="button__next">
         START
       </button>
