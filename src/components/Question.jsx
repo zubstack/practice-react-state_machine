96424a60ca2fdc03ebac6caddd1f5c8d31cf5623
Ariel Alejandro <mt.calderon.alejandro@gmail.com>
2023-09-21 10:49:25 -0500

    [implement]: questions from context

--- src/components/Question.jsx
+++ src/components/Question.jsx
@@ -3,27 +3,30 @@ import { useState } from "react";
 import Nav from "./Nav";
 import "./Question.css";
 
-function Question({ send, number, questions }) {
+function Question({ state, send, number, questions }) {
+  const questionIndex = number - 1;
+  const currentQuestion = state.context.questions[questionIndex];
+  console.log("currentQuestion", currentQuestion);
+
   const lastQuestion = number === questions.length;
   const action = lastQuestion ? "FINISH" : "CONTINUE";
-  const questionIndex = number - 1;
   const [currentValue, setCurrentValue] = useState("*");
-  if (!currentValue) {
-    console.log("Not answered");
-  } else {
-    console.log(
-      currentValue === questions[questionIndex].answerIndex
-        ? "Correct answer"
-        : "Incorrect"
-    );
-  }
+  // if (!currentValue) {
+  //   console.log("Not answered");
+  // } else {
+  //   console.log(
+  //     currentValue === questions[questionIndex].answerIndex
+  //       ? "Correct answer"
+  //       : "Incorrect"
+  //   );
+  // }
 
   return (
     <>
       <h3>Question #{questions[questionIndex].number}</h3>
       <div className="question__container">
-        <h4>{questions[questionIndex].question}</h4>
-        {questions[questionIndex].options.map((option, index) => (
+        <h4>{currentQuestion.question}</h4>
+        {currentQuestion.alternatives.map((option, index) => (
           <div key={index}>
             <input
               onChange={(e) => setCurrentValue(parseInt(e.target.id))}
